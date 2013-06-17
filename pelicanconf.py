80bbb4aebdb7bb52c9744ac3ee3a42188497ef50
Rich Wareham <rjw57@cam.ac.uk>
2013-06-17 16:01:03 +0100

    switch to octopress theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,6 +25,8 @@ LINKS =  (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
+THEME = 'themes/octopress'
+
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
