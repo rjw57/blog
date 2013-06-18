ac4c0082e8b6de7e67a9a0b5de982e76f27fed39
Rich Wareham <rjw57@cam.ac.uk>
2013-06-18 11:09:08 +0100

    some teaks to article URL structure

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,6 +10,13 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
+# Make the article URLs a little nicer
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+# Add an explicit 'downloads' static URL location
+STATIC_PATHS = ['images', 'downloads']
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
